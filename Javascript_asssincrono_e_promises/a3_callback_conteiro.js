5c45e04d6c0c719e1251458ae97a45e25415d0e5
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-08-24 17:08:24 -0300

    Callback
     Quando damos a uma função como argumento outra função que logo vai ser chamada de volta

--- Javascript_asssincrono_e_promises/a3_callback_conteiro.js
+++ Javascript_asssincrono_e_promises/a3_callback_conteiro.js
@@ -1,4 +1,9 @@
 // Funções aceitam qualuqer tipo de dado como argumento
 function imprimirDado(dado) {
-  console.log(dado)
+  console.log('outras tarefas')
+  console.log(dado())
 }
+
+imprimirDado(function() {
+  return 'Ola mundo'
+})
