90c1ca2b692eea252bf023d65779d837c9ac27c6
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-08-31 15:09:36 -0300

    copy cod

--- Javascript_asssincrono_e_promises/a7_promises_no_código.js
+++ Javascript_asssincrono_e_promises/a7_promises_no_código.js
@@ -5,3 +5,12 @@
   * Poderá dar certo ou errado,
   * mas irá acontecer
 */
+
+let aceitar = false 
+
+console.log('pedir uber')
+const promessa = new Promise((resolve, reject)) => {
+  return reject('carro chegou')
+}
+
+console.log('aguardando')
