7170a273679cb6658840fb0b70573b644fc0106c
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-26 16:08:56 -0300

    Poder da arrow para devolver tudo somente em uma linha

--- Javascript_asssincrono_e_promises/a12_promises_com_axios.js
+++ Javascript_asssincrono_e_promises/a12_promises_com_axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 axios.get('https://api.github.com/users/maykbrito')
-  .then(response => {
-    console.log(response.data);
-  })
+  .then(response => axios.get(response.data.repos_url))
+  .then(repos => console.log(repos.data))
   .catch(error => console.log(error))
+
+// COdigo axios reduzido e com o mesmo funcionamento
