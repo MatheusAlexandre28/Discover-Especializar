ea78b13f6cd799cd3f7feb557c1c7fcf5e58f49c
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-30 16:54:45 -0300

    Forma melhor
    async function start podemos pegar toda aquela promessa abaixo e colocar em uma funcao assincrona para que possamos rodar mais corretamente com try catch e finally ao inves de usar o .then.

--- Javascript_asssincrono_e_promises/a15_async_await.js
+++ Javascript_asssincrono_e_promises/a15_async_await.js
@@ -3,13 +3,27 @@ const promessa = new Promise( function( resolve, reject ) {
   return resolve('ok')
 })
 
-promessa
-  .then(function(response){
-    console.log(response)
-  })
-  .catch(function (error){
-    console.log(error)
-  })
-  .finally( function(){
-    console.log('sempre irei executar')
-  })
+async function start(){
+  try {
+    const result = await promessa
+    console.log(result)
+  } catch(e){
+    console.log(e)
+  } finally{
+    console.log('rodar sempre')
+  }
+
+}
+
+start()
+
+// promessa
+//   .then(function(response){
+//     console.log(response)
+//   })
+//   .catch(function (error){
+//     console.log(error)
+//   })
+//   .finally( function(){
+//     console.log('sempre irei executar')
+//   })
