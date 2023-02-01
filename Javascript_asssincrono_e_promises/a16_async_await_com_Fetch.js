168cca8f70c8f99ef7eb37e7ddc2da24193acde6
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-02-01 15:39:40 -0300

    segunda forma de capturar o catch

--- Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
+++ Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
@@ -30,3 +30,14 @@ async function start(){
 }
 
 start()
+
+// ou deste jeito
+async function start(){
+  const response = await fetch('https://api.github.com/users/maykbrito')
+  const user = await response.json()
+  const reposResponse = await fetch(user.repos_url)
+  const repos = await reposResponse.json()
+  console.log(repos)
+}
+
+start().catch(e => console.log(e))
