41823c6921b9a0aaebc88f760cd831bb0ae1c63a
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-02-01 15:32:38 -0300

    Ideia de await e com o try

--- Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
+++ Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
@@ -5,10 +5,28 @@
 // .then( d => console.log(d))
 // .catch( err => console.log(err))
 
+//ent parece q o codigo fica com uma maneira mais sincrona, este é o grande lance do async await, transformar um codigo q antes tinha encadeamento em uma ideia um pouco mais sincrona.
+
 async function start(){
   const response = await fetch('https://api.github.com/users/maykbrito')
   const user = await response.json()
   const reposResponse = await fetch(user.repos_url)
+}
+
+start()
+
+// Tbm podemos usar este codigo mais sincrono com o try catch para pegar o erro
+async function start(){
+  try {
+    const response = await fetch('https://api.github.com/users/maykbrito')
+    const user = await response.json()
+    const reposResponse = await fetch(user.repos_url)
+    const repos = await reposResponse.json()
+    console.log(repos)
+  }
+  catch(e){
+    console.log(e);
+  }
 }
 
 start()
