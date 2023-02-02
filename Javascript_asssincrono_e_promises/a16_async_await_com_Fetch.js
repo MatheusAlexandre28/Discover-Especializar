316d8db3411c04d4c979b86562e6f1272762ecf7
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-02-02 15:47:36 -0300

    para deixar codigo mais legivel
    tirando a url do fetch e colocando em uma constante

--- Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
+++ Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
@@ -50,3 +50,13 @@ async function start(){
 }
 
 start().catch(e => console.log(e))
+
+// jeito mais legivel do codigo
+async function start(){
+  const url = "https://api.github.com/users/maykbrito"
+  const user = await fetch(url).then(r => r.json())
+  const repos = await fetch(user.repos_url).then(r => r.json())
+  console.log(repos)
+}
+
+start().catch(e => console.log(e))
