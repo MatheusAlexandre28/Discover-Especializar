3c453e9bd04b8846385cbfd83bb908e213d4f4c7
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-02-06 19:10:44 -0300

    função iniciciada

--- Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
+++ Javascript_asssincrono_e_promises/a16_async_await_com_Fetch.js
@@ -6,7 +6,9 @@
 // .catch( err => console.log(err))
 
 async function start(){
-
+  const response = await fetch('https://api.github.com/users/maykbrito')
+  const user = await response.json()
+  const reposResponse = await fetch(user.repos_url)
 }
 
 start()
