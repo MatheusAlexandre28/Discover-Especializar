71b8a46c5b1a757327cac3684b7e9b9d41b0e8ed
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-06 16:43:09 -0300

    Aprendemos a modificar o conteudo de uma var
    Pelo insomnia conseguimos mudar o conteudo de uma variavel, apenas iniciando a aplicação rodando o PUT no insomnia e depois rodando o GET assim aparecendo na tela oq fizemos de alteração

--- Trabalhando_com_APIs/2_API_no_Back-end/APIs/index.js
+++ Trabalhando_com_APIs/2_API_no_Back-end/APIs/index.js
@@ -4,11 +4,18 @@ const app = express()
 
 app.listen('3000')
 
+//middleware
 app.use(express.json())
 
-let author = "Matheus Alexandre"
+let author = "Matheus"
+
+app.route('/').get( (req, res) => res.send(author))
 
 app.route('/').put( (req, res) => {
   author = req.body
   res.send(author)
 } )
+
+{
+  "author":"Matheus Alexandre"
+}
