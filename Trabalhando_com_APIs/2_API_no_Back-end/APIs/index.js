94ebd1da10e29341d2313b6d83a0e246527211fb
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-09 10:58:10 -0300

    Usei axios
    Baixamos ele com npm i axios e após colocamos ele em uma variavel, dentro do express para pegar a rota usamos ele para usarmos uma api externa que no caso foi a do github, pegamos as informaçoes e ate mesmo a img do git

--- Trabalhando_com_APIs/2_API_no_Back-end/APIs/index.js
+++ Trabalhando_com_APIs/2_API_no_Back-end/APIs/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 
 const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req, res) => res.send( req.query.nome ))
-
-app.route('/about/user').get((req, res) => res.send( req.query.id ))
-
-// Aula no insomnia sobre github
+app.route('/').get((req, res) => {
+  axios.get('https://api.github.com/users/MatheusAlexandre28')
+  .then(result => res.send(`<img src="${result.data.avatar_url}"/>`) )
+  .catch(error => console.log(error))
+})
