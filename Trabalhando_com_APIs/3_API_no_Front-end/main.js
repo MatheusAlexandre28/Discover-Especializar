2c165aa4aba98c2b075bcffb15cfad501d1e84d3
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-10 11:08:42 -0300

    Aprendemos get em parametros
    Conseguimos pegar tags e usar elas no script assim podendo modificar tags e tudo mais apenas no script

--- Trabalhando_com_APIs/3_API_no_Front-end/main.js
+++ Trabalhando_com_APIs/3_API_no_Front-end/main.js
@@ -1,10 +1,26 @@
 const url="http://localhost:5500/api"
 
-function getUsers(){
+function getUsers() {
   fetch(url)
     .then(response => response.json())
     .then(data => renderApiResult.textContent = JSON.stringify(data))
     .catch(error => console.log(error))
 }
 
+function getUser() {
+  fetch(`${url}/1`)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userCity.textContent = data.city
+      userAvatar.src = data.avatar
+    })
+    .catch(error => console.log(error))
+}
+
+function addUser(){
+  
+}
+
 getUsers()
+getUser()
