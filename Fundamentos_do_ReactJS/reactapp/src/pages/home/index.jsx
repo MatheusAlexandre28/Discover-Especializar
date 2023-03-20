d212c0be44a6f2af02ea33230a6f6744d829a15f
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-20 11:37:56 -0300

    CSS
    Estilizamos nosso css

--- Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
+++ Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
@@ -2,10 +2,10 @@ import './styles.css'
 
 export function Home() {
   return (
-    <>
-    <h1>Lista de presença</h1>
-    <input type="text" placeholder='Digite um nome...'/>
-    <button type='button'>Adicionar</button>
-    </>
+    <div className='container'>
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder='Digite um nome...'/>
+      <button type='button'>Adicionar</button>
+    </div>
   )
 }
