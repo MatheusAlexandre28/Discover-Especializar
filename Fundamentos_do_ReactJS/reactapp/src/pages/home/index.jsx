855b217943b10c7a4f05a202da389d19d07e921f
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-22 20:32:58 -0300

    Estado
    Agr queremos digitar o nome no input armazenar este nome em algum lugar e quando clicarmos em adicionar vamos adicionar essa pessoa a nossa lista

--- Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
+++ Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './styles.css'
 
 import { Card } from '../../components/Card';
@@ -5,8 +6,14 @@ import { Card } from '../../components/Card';
 export function Home() {
   return (
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder='Digite um nome...'/>
+      <h1>Nome: {studentName}</h1>
+
+      <input 
+        type="text" 
+        placeholder='Digite um nome...'
+        onChange={e => handleNameChange(e.target.value)}
+      />
+
       <button type='button'>Adicionar</button>
 
       <Card name="Matheus" time="10:55:25"/>
