b119b94a3691db617d8553597bbea3de6670324e
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-03-24 11:42:38 -0300

    Falta resolver bug
    Agr para cada estudante conseguimos renderizar um cartãozinho. Passamos o idioma padrão e o objeto com as config de horas

--- Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
+++ Fundamentos_do_ReactJS/reactapp/src/pages/home/index.jsx
@@ -7,6 +7,18 @@ export function Home() {
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
 
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+    setStudents([newStudent]);
+  }
+
   return (
     <div className='container'>
       <h1>Lista de Presença:</h1>
@@ -17,10 +29,12 @@ export function Home() {
         onChange={e => setStudentName(e.target.value)}
       />
 
-      <button type='button'>Adicionar</button>
+      <button type='button' onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
       {
-        students.map(student => <Card name="Matheus" time="10:55:25"/>)
+        students.map(student => <Card name={student.name} time={student.time}/>)
       }
     </div>
   )
